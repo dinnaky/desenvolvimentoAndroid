e1b52e4eba03e42f310e58fe65d4aa0e83062f87
Arthur Vitor <arhturvitorsilvio@gmail.com>
2024-03-26 21:36:08 -0300

    Changes in buildGradle

--- appListaCurso/app/build.gradle.kts
+++ appListaCurso/app/build.gradle.kts
@@ -12,8 +12,6 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -34,7 +32,4 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
 }
